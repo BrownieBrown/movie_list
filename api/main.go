4830695bcbdc4bb689b100c532ca8f019cedbe58
Marco Braun <marco_alexander_braun@proton.me>
2024-10-20 00:45:51 +0200

    Feat: Add Players Page and Navbar

--- api/main.go
+++ api/main.go
@@ -5,11 +5,18 @@ import (
 	"movie_list/api/pkg/controller"
 	"movie_list/api/pkg/service"
 
+	"github.com/gofiber/fiber/v2/middleware/cors"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173", // Update with your frontend's origin
+		AllowMethods: "GET,POST,DELETE",
+	}))
+
 	playerController := controller.NewPlayerController(service.NewPlayerService())
 
 	api := app.Group("/api")
